43c3390386eceb5a8e6fa749984bd9e717399ef7
cmuth001 <cmuth001@odu.edu>
2019-08-05 00:21:38 -0400

    mongoDB connected to angular app!

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-
+const mongoose = require('mongoose');
+const Post = require('./models/post');
+mongoose.connect("mongodb+srv://cmuth001:uIQc0vhkYeHTbzkI@chat-room-eqvp3.mongodb.net/chat-room?retryWrites=true&w=majority", { useNewUrlParser: true })
+.then( () => {
+  console.log("Database connected!");
+})
+.catch( () => {
+  console.log('Failed in connecting!');
+});
+// uIQc0vhkYeHTbzkI
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(function(req, res, next) {
@@ -10,20 +19,26 @@ app.use(function(req, res, next) {
   next();
 });
 app.post('/api/posts', (req, res, next) => {
-  const post = req.body;
-  console.log(post);
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  // const post = req.body;
+  post.save();
+  // console.log(post);
   res.status(201).json({
     message: 'post added in to server',
   });
 });
 app.use('/api/posts', (req, res, next)=>{
-  const posts = [
-    {id: 'aasdfghjkl1', title: 'title1', content: 'this is my content1'},
-    {id: 'aasdfghjkl1', title: 'title2', content: 'this is my content2'},
-    {id: 'aasdfghjkl1', title: 'title3', content: 'this is my content3'},];
-  res.status(200).json({
-    message: 'posts sent successfully',
-    posts: posts,
-  });
+  Post.find()
+  .then( (result) =>{
+    res.status(200).json({
+      message: 'posts sent successfully',
+      posts: result,
+    });
+  })
+  .catch();
+
 });
 module.exports = app;
